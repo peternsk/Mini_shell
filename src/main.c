302ccbcd78cce62844dd0da337c39061fffe86fc
Peter Malonge Nsaka <pnsaka@c1r8p4.42quebec.com>
2024-05-10 15:28:14 -0400

    vendredi am 10 mai 42 : here document done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 18:20:31 by peternsaka        #+#    #+#             */
-/*   Updated: 2024/05/10 14:15:19 by pnsaka           ###   ########.fr       */
+/*   Updated: 2024/05/10 15:01:16 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int		main(int ac, char **av, char **env)
 			// printRealList(m_s->token_lst);
 			ft_cmdBuilder(m_s, &m_s->token_lst, &m_s->cmdLst);
 			print_cmdLst(&m_s->cmdLst);
-			// init_cmds(env, m_s);
+			init_cmds(env, m_s);
 			// print_garbage_collector();
 			// m_s->tab_env = list_to_tab(&m_s->envVarlst);
 			
